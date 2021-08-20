60ebe7e05c0043ed4a03c089f5612e4c43f3ce50
Rrojin11 <dbwls3225@naver.com>
2021-08-20 14:49:30 +0900

    0820

---
+++ BaekJoon_Basic/10845.py
@@ -0,0 +1,32 @@
+import sys
+n = int(sys.stdin.readline())
+
+if __name__ == '__main__':
+    s = []
+    for _ in range(n):
+        a = sys.stdin.readline().split()
+        if a[0]=='push':
+            s.append(a[1])
+        elif a[0]=='pop':
+            if len(s)==0:
+                print(-1)
+            else:
+                print(s.pop(0))
+        elif a[0]=='size':
+            print(len(s))
+        elif a[0]=='empty':
+            if len(s)==0:
+                print(1)
+            else:
+                print(0)
+        elif a[0]=='front':
+            if len(s)==0:
+                print(-1)
+            else:
+                print(s[0])
+        elif a[0]=='back':
+            if len(s)==0:
+                print(-1)
+            else:
+                print(s[-1])
+    #print(s)
