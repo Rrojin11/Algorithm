6221b075727008bd7c6ca3b4342c0458f1cdb941
Rrojin11 <dbwls3225@naver.com>
2021-08-20 15:38:41 +0900

    0820

---
+++ BaekJoon_Basic/1158.py
@@ -0,0 +1,19 @@
+import sys
+# ''.join(list) : list내 요소가 문자여야함.
+if __name__=='__main__':
+    n,k = map(int, sys.stdin.readline().split())
+    k = k-1
+    a = list(range(1,n+1))
+    cur = 0
+    ans = []
+    for _ in range(n):
+        s = len(a)
+        t = (cur+k)%s
+        ans.append(str(a.pop(t)))
+        cur = t
+
+    print('<',end='')
+    print(', '.join(ans),end = '')
+    print('>')
+
+
