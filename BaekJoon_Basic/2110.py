1fc21944836fd2eced647ed8da52c54944b7060a
YoojinKim <43817290+Rrojin11@users.noreply.github.com>
2024-09-10 09:37:52 +0900

    Create 2110.py

---
+++ BaekJoon_Basic/2110.py
@@ -0,0 +1,30 @@
+n, c = map(int, input().split())
+array = []
+for _ in range(n):
+  array.append(int(input()))
+array.sort()
+
+start, end = 1, max(array) - min(array)
+
+
+def check(val):
+  now = 1
+  tmp = 1
+  prev = 0
+  while now < n:
+    if array[prev] + val <= array[now]:
+      tmp += 1
+      prev = now
+    now += 1
+  return tmp
+
+
+result = 0
+while start <= end:
+  mid = (start + end) // 2
+  if check(mid) >= c:
+    result = mid
+    start = mid + 1
+  else:
+    end = mid - 1
+print(result)
