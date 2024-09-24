b9189a5e367f8c24bc7e1d7f968a697112ba96ba
YoojinKim <43817290+Rrojin11@users.noreply.github.com>
2024-09-24 23:05:21 +0900

    Create 345. Reverse Vowels of a String.py

---
+++ LeetCode/345. Reverse Vowels of a String.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = 'aeiouAEIOU'
+        tmp = [v for v in s if v in vowels]
+
+        answer = ''
+
+        for v in s:
+            if v in vowels:
+                answer+=(tmp.pop())
+            else:
+                answer+=v
+
+        
+        return answer
