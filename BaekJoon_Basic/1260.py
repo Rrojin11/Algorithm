74a568f706c20e397ac3b81730af3f8d32778606
YoojinKim <43817290+Rrojin11@users.noreply.github.com>
2024-10-07 23:26:40 +0900

    Create 1260.py

---
+++ BaekJoon_Basic/1260.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+n,m,v = map(int, input().split())
+a = [ [] for _ in range(n+1)]
+for _ in range(m):
+    s,e = map(int,input().split())
+    a[s].append(e)
+    a[e].append(s)
+
+check = [False]*(n+1)
+
+for k in range(1,n+1):
+    a[k] = sorted(a[k])
+def dfs(v):
+    check[v]=True
+    print(v, end = ' ')
+    for i in a[v]:
+        if check[i]==False:
+            #방문
+            dfs(i)
+
+check1 = [False]*(n+1)
+dq = deque()
+def bfs(v):
+    check1[v]=True
+    print(v, end=' ')
+    dq.append(v)
+    while dq:
+        k = dq.popleft()
+        for i in a[k]:
+            if check1[i]==False:
+                check1[i]=True
+                print(i, end=' ')
+                dq.append(i)
+
+
+
+dfs(v)
+print()
+bfs(v)
